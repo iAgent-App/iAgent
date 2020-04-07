d0651c641fbf854e5081f4a78178f022ded7c240
Daniel Hart <43150766+danielhart17@users.noreply.github.com>
2020-04-07 17:30:07 -0400

    new commit

---
+++ iAgent/iAgentTests/iAgentTests.swift
@@ -0,0 +1,34 @@
+//
+//  iAgentTests.swift
+//  iAgentTests
+//
+//  Created by Daniel Hart on 4/7/20.
+//  Copyright © 2020 Daniel Hart. All rights reserved.
+//
+
+import XCTest
+@testable import iAgent
+
+class iAgentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
