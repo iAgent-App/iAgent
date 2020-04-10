69af5c89f4415a4aa0b0dd60dfd8400d3ab94b40
Daniel Hart <43150766+danielhart17@users.noreply.github.com>
2020-04-09 20:12:05 -0400

    added signup screen 1, 2 and athlete signup

---
+++ iAgent/iAgent/CreateAccount1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccount1ViewController.swift
+//  iAgent
+//
+//  Created by Daniel Hart on 4/9/20.
+//  Copyright © 2020 Daniel Hart. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccount1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
