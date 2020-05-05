7e35bdd6288201eb6005719fdac387fa8f3b9004
Lailah <lmweatherspoon0426@gmail.com>
2020-05-04 22:45:20 -0500

    connection profile page

---
+++ iAgent/iAgent/ConnectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ConnectionViewController.swift
+//  iAgent
+//
+//  Created by user167032 on 5/4/20.
+//  Copyright © 2020 Daniel Hart. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionViewController: UIViewController {
+
+    @IBOutlet weak var addConnectionButton: UIButton!
+    @IBOutlet weak var connectionStatsButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
